bc554c886072a76b7bc303167f9e19e72f49e40c
Kollivakkam Raghavan <rkolliva@cisco.com>
2016-09-17 10:03:21 -0700

    Module 2 Solution

---
+++ module2-solution/app.js
@@ -0,0 +1,78 @@
+/**
+ * Created by rkolliva
+ * on 9/11/16.
+ */
+
+(function () {
+
+  'use strict';
+
+  angular.module("ShoppingCartApp", [])
+         .controller('ItemsToBuyController', ItemsToBuyController)
+         .controller('BoughtItemsController', BoughtItemsController)
+         .service('ItemsService', ItemsService);
+
+  var randomString = function (len) {
+    var text = "";
+    len = len || 10;
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < len; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    // make sure it starts with an alphabet.
+    return 'a' + text;
+  };
+
+  ItemsToBuyController.$inject = ['ItemsService'];
+  function ItemsToBuyController(itemsService) {
+    var me = this;
+    me.itemsToBuy = itemsService.getItemsToBuy();
+
+    me.buyItem = function (index) {
+      itemsService.buyItem(index);
+    }
+  }
+
+  BoughtItemsController.$inject = ['ItemsService'];
+  function BoughtItemsController(itemsService) {
+    var me = this;
+    me.boughtItems = itemsService.getBoughtItems();
+  }
+
+  function ItemsService() {
+    var me                      = this,
+        itemsToBuy              = [],
+        boughtItems             = [],
+        // generate between 5 and 7 initial items
+        randomCountOfItemsToBuy = Math.floor(5 + Math.random() * 2);
+
+    for (var i = 0; i < randomCountOfItemsToBuy; i++) {
+      itemsToBuy.push({
+                        name: randomString(10),
+                        quantity: Math.floor(Math.random() * 10) + 1
+                      });
+    }
+
+    this.getItemsToBuy = function () {
+      return itemsToBuy;
+    };
+
+    this.getBoughtItems = function () {
+      return boughtItems;
+    };
+
+    this.buyItem = function (indexOfItem) {
+      var item;
+      if (indexOfItem >= 0 && indexOfItem < itemsToBuy.length) {
+        item = itemsToBuy[indexOfItem];
+
+        itemsToBuy.splice(indexOfItem, 1);
+        boughtItems.push(item);
+      }
+    };
+
+  }
+
+})();
